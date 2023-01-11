eac95010b0f798ed19fdffd03a143bc54b8d4887
jl8771 <jlxpi123@gmail.com>
2023-01-11 14:24:34 -0800

    Update with comments based on working AWS Elastic Beanstalk app

--- application.py
+++ application.py
@@ -5,40 +5,44 @@ import pickle
 import datetime
 import boto3
 
-#Import packages for dashboarding
+#Import packages for building the dashbaord
 from dash import Dash, dash_table, html, dcc, Input, Output
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.subplots as sp
 
+#Set boto3 resource to access s3 buckets where the largest data is stored in pkl format
 s3 = boto3.resource('s3')
 
-#df1 = pd.read_csv('https://jackyluo.s3.amazonaws.com/AirlineData.csv', parse_dates=['FL_DATE', 'CRS_DEP_TIME', 'DEP_TIME', 'WHEELS_OFF', 'CRS_ARR_TIME', 'ARR_TIME', 'WHEELS_ON'])
+#Using pickle load from s3 as it parses much faster than reading csv
 df1 = pickle.loads(s3.Bucket('jackyluo').Object('AirlineData.pkl').get()['Body'].read())
+#Read remaining csvs
 df2 = pd.read_csv('https://jackyluo.s3.amazonaws.com/AircraftData.csv')
 df3 = pd.read_csv('https://jackyluo.s3.amazonaws.com/Stations.csv')
 df4 = pd.read_csv('https://jackyluo.s3.amazonaws.com/Carriers.csv', index_col='Code')
     
+#Merge airline data with aircraft data to add aircraft information
 data = pd.merge(df1, df2, how='left', left_on='TAIL_NUM', right_on='Tail')
 
+#Create methods for conversion between unique carrier codes and carrier names for readability
 carrier_code_to_name_converter = df4.to_dict()['Description']
 carrier_name_to_code_converter = {value: key for key,value in carrier_code_to_name_converter.items()}
 carrier_types = ['MKT_UNIQUE_CARRIER', 'OP_UNIQUE_CARRIER']
 carrier_type = carrier_types[0]
 
+#Create alphabetical list of carriers for use in dropdown
 op_carriers = list(df1[carrier_type].unique())
 op_carriers = [carrier_code_to_name_converter[x] + ' (' + x + ')' for x in op_carriers]
 op_carriers = sorted(op_carriers)
 
+#Stylesheet
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+#Set app
 app = Dash(__name__, update_title='Loading...', external_stylesheets=external_stylesheets)
-#app.title = '2022 Bureau of Transportation Statistics Airline Data Visualization'
-
 application = app.server
 
-#Clear the layout and do not display exception till callback gets executed
-
+#Set initial layout, including input elements and placeholder divs
 app.layout = html.Div([
     html.Div(id='title-changer'),
     html.Div([
@@ -71,6 +75,7 @@ app.layout = html.Div([
     ], style={'display':'block'})
 ])
 
+#Callback 1 for disabling tabs until carrier has been selected
 @app.callback(
     Output('tab0', 'disabled'),
     Output('tab1', 'disabled'),
@@ -78,10 +83,19 @@ app.layout = html.Div([
     Input('main-dropdown', 'value')
 )
 def enable_tabs(airline):
+    """Disable tabs until a carrier has been selected in the dropdown.
+
+    Args:
+        airline (str): Carrier selected by the dropdown.
+
+    Returns:
+        tuple (boolean, boolean, boolean): Enable/disable boolean for aircraft/network/performance data tab respectively
+    """
     if airline:
         return False, False, False
     return True, True, True
 
+#Callback 2 for generating output based on tab selected
 @app.callback(
     Output('output-wrapper', 'children'),
     Input('main-dropdown', 'value'),
@@ -90,41 +104,75 @@ def enable_tabs(airline):
     Input('main-type-selector', 'value')
 )
 def update_airline(airline, date_range, tab_selected, selected_type):
+    """Generates the output dash html elements including graphs, tables and text based on selected carrier, date range, tab and operating mode.
+
+    Args:
+        airline (str): Selected carrier from dropdown input.
+        date_range (list of int): Selected date range in months, min and max val from rangeslider input. Default [1,9] based on currently available data.
+        tab_selected (str): Selected type of data to be shown from tabs input. 
+        selected_type (str): Selected operating mode for aircraft type from radioitems input. Default "General Type".
+
+    Returns:
+        dash.Html.Div: Wrapper for output. Contains graphs, tables and text.
+    """
+    #If a carrier is selected, get its unique carrier code
     if airline:
         airline = carrier_name_to_code_converter[airline[:-5]]
+    #Filter data to be only from the selected carrier
     df = data[data[carrier_type] == airline]
+    #Create date range based on range selected by the slider
     date_range[0] = '0' + str(date_range[0]) if date_range[0] < 10 else str(date_range[0])
     date_range[1] = '0' + str(date_range[1] + 1) if date_range[1] < 9 else str(date_range[1] + 1)
     date_range[0] = '2022-' + date_range[0]
     date_range[1] = '2022-' + date_range[1] if date_range[1] != '13' else '2023-01'
     dates = np.arange(date_range[0], date_range[1], dtype='datetime64[D]')
+    #Get min date and max date in numpy datetime64 type
     start_date = dates[0]
     end_date = dates[len(dates) - 1]
+    #Filter data to be only within date range
     df = df[(df['FL_DATE'] >= start_date) & (df['FL_DATE'] <= end_date)]
+    #Initialize output as empty div
     output = html.Div()
     
+    #TODO: Try using df for each tab type rather than create copies to reduce memory footprint. Test if any effect on loading speed for each tab.
+    #Output relevant aircraft data if aircraft data tab is selected
     if tab_selected == 'Aircraft Data':
+        #Set operating type based on selected operating type
         op_types = ['General Type', 'ICAO Type']
         op_type = op_types[0] if selected_type == 'Aircraft use General Type Designators' else op_types[1]
+        #Create a copy of the data
         df_aircraft = df.copy()
+        #Filter by operating carrier rather than marketing carrier to prevent overlapping fleet information from regional carriers.
+        #This also allows the fleet data to be compared to other fleet tracking websites for consistency.
         df_aircraft = df_aircraft[df_aircraft['OP_UNIQUE_CARRIER'] == airline]
+        #Create a dataframe of unique N-numbers, which uniquely identify aircraft
         df_aircraft = df_aircraft[~df_aircraft['Tail'].duplicated(keep='first')]
         df_aircraft['Range'] = None
+        #Create a single column for range of aircraft
         df_aircraft['Range'] = df_aircraft.apply(lambda x: 'Short Range' if x['Short Range'] == 1 else x['Range'], axis=1)
         df_aircraft['Range'] = df_aircraft.apply(lambda x: 'Medium Range' if x['Medium Range'] == 1 else x['Range'], axis=1)
         df_aircraft['Range'] = df_aircraft.apply(lambda x: 'Long Range' if x['Long Range'] == 1 else x['Range'], axis=1)
+        #Create a single column for wide or narrowbody aircraft
         df_aircraft['Width'] = df_aircraft.apply(lambda x: 'Wide-body' if x['Wide-body'] == 1 else 'Narrow-body', axis=1)
         df_aircraft = df_aircraft[['Range', 'Width', 'Manufacturer', 'ICAO Type', 'General Type', 'Tail', 'Year']]
+        #Create a counter column for building fleet size
         df_aircraft['Count'] = 1
 
+        #Group aircraft by type to determine number of aircraft of each type as a dataframe, rename the appropriate column and reset index for merge
         df_fleet_0 = df_aircraft.groupby(op_type)[op_type].count().to_frame().rename({op_type: 'Number of Aircraft'}, axis=1).reset_index()
+        #Group aircraft by type to determine average age of aircraft of each type as a dataframe, rename the appropriate column and reset index for merge
         df_fleet_1 = df_aircraft.groupby(op_type)['Year'].mean().to_frame().rename({'Year': 'Average Age'}, axis=1).reset_index()
+        #Calculate the age using current datetime, fix float precision
         df_fleet_1['Average Age'] = float(datetime.datetime.now().year) - df_fleet_1['Average Age']
         df_fleet_1['Average Age'] = df_fleet_1['Average Age'].round(2)
+        #Perform the merge to have a single dataframe with aircraft type, number of aircraft of given type, and average age of given type
         df_fleet = pd.merge(df_fleet_0, df_fleet_1, how='inner', left_on=op_type, right_on=op_type)
+        #Initially sort by descending number of aircraft to have most common aircraft at the top and least common aircraft at the bottom
         df_fleet = df_fleet.sort_values(by=['Number of Aircraft', op_type], ascending=False)
+        #Get total fleet size by summing number of aircraft of each type
         total_fleet_size = df_fleet['Number of Aircraft'].sum()
         
+        #Create output element, including sortable datatable and plots. Use class names to fix element widths based on stylesheet
         output = html.Div([
             html.Div([
                 html.Div([
@@ -159,29 +207,45 @@ def update_airline(airline, date_range, tab_selected, selected_type):
                 ]),
             ]),
         ])
+    #Output relevant network data if network data tab is selected
     elif tab_selected == 'Network Data':
+        #Create a copy of the data
         df_network = df.copy()
+        #Reindex data to make data easier to work with
         df_network = df_network.reset_index()
+        #Get list of unique operators, convert to names instead of unique codes
         operators = df_network['OP_UNIQUE_CARRIER'].unique()
         operators = [carrier_code_to_name_converter[x] for x in operators]
         
+        #Get number of flights by each origin airport location
         num_flights = df_network.groupby('ORIGIN')['ORIGIN'].count().to_frame().rename({'ORIGIN': 'Number of Flights'}, axis=1).reset_index()
+        #Find the distance of longest flight, get index of longest flight to get other data such as airtime, origin and destination
         longest_flight = df_network['DISTANCE'].max()
         longest_flight_idx = df_network['DISTANCE'].idxmax()
         longest_flight_airtime = int(df_network['AIR_TIME'].iloc[longest_flight_idx])
         longest_flight_pair = (df_network['ORIGIN'].iloc[longest_flight_idx], df_network['DEST'].iloc[longest_flight_idx])
+        #Find the mean distance and airtime as comparison for shortest/longest flights
         mean_distance = df_network['DISTANCE'].mean()
         mean_airtime = df_network['AIR_TIME'].mean()
+        #Find the distance of shortest flight, get index of shortest flight to get other data such as airtime, origin and destination
         shortest_flight = df_network['DISTANCE'].min()
         shortest_flight_idx = df_network['DISTANCE'].idxmin()
         shortest_flight_airtime = int(df_network['AIR_TIME'].iloc[shortest_flight_idx])
         shortest_flight_pair = (df_network['ORIGIN'].iloc[shortest_flight_idx], df_network['DEST'].iloc[shortest_flight_idx])
+        #Get number of total unique destinations
         total_destinations = df_network['DEST'].nunique()
+        #Filter data by unique origin/destination pairs for mapping
         df_network = df_network[~df_network[['ORIGIN', 'DEST']].duplicated(keep='first')]
+        #Merge origin/destination pair data with airport names along with longitude/latitude/elevation data
         df_network = pd.merge(df_network, df3, how='inner', left_on='ORIGIN', right_on='ORIGIN')
+        #Merge origin/destination pair data with number of flights from each destination calculated earlier
         df_network = pd.merge(df_network, num_flights, how='inner', left_on='ORIGIN', right_on='ORIGIN')
+        #Get top 10 airports by number of outgoing flights, should represent hub airports
         hubs = df_network[~df_network['ORIGIN'].duplicated(keep='first')].sort_values('Number of Flights').tail(10)['ORIGIN']
         
+        #TODO: Reformat text elements to be table-like with descriptive text as small header, numerical value as large table element for cleaner look
+        #TODO: Figure out px.line_mapbox to create route map that includes lines between each origin/destination pair. Currently not clean, incompatible with px.scatter_mapbox
+        #Create output element, including text and map. Use class names to fix element widths based on stylesheet
         output = html.Div([
             html.Div([
                 html.Div([
@@ -209,34 +273,41 @@ def update_airline(airline, date_range, tab_selected, selected_type):
                 ]),
             ], className='twelve columns'),
         ])
+    #Output relevant performance data if performance data tab is selected
     elif tab_selected == 'Performance Data':
+        #Create a copy of the data
         df_routes = df.copy()
-        days_of_week = df_routes.groupby('DAY_OF_WEEK')['ORIGIN'].count().to_frame().reset_index()
-        days_of_week['Day Name'] = days_of_week['DAY_OF_WEEK']
-        days_of_week.replace({'Day Name': {1:'Monday',2:'Tuesday',3:'Wednesday',4:'Thursday',5:'Friday',6:'Saturday',7:'Sunday',9:'Unknown'}}, inplace=True)
-        days_of_week.rename({'DAY_OF_WEEK': 'Day of Week', 'ORIGIN': 'Number of Flights'}, axis=1, inplace=True)
+        #Create a dataframe with the average arrival delay and departure delay by day of year
         daily_avg_delay_0 = df_routes.groupby('FL_DATE')['ARR_DELAY'].mean().to_frame().reset_index()
         daily_avg_delay_1 = df_routes.groupby('FL_DATE')['DEP_DELAY'].mean().to_frame().reset_index()
         daily_avg_delay = pd.merge(daily_avg_delay_0, daily_avg_delay_1, how='inner', left_on='FL_DATE', right_on='FL_DATE')
+        #Get average arrival delay and departure delay for all routes by the selected carrier
         dep_delay = df_routes['DEP_DELAY'].mean()
         arr_delay = df_routes['ARR_DELAY'].mean()
+        #Get percentage of flights where flights are on time or early (delay <= 0)
         ontime_arr = df_routes[df_routes['ARR_DELAY'] <= 0].shape[0] / df_routes.shape[0] * 100
         ontime_dep = df_routes[df_routes['DEP_DELAY'] <= 0].shape[0] / df_routes.shape[0] * 100
-        ontime_conv = df_routes[(df_routes['DEP_DELAY'] > 0) & (df_routes['ARR_DELAY'] <= 0)].shape[0] / df_routes[df_routes['DEP_DELAY'] <= 0].shape[0] * 100
+        #Create a list of delay causes
         delays = []
+        #Sum number of minutes of delay by each cause
         delay0 = df_routes['CARRIER_DELAY'].sum()
         delay1 = df_routes['WEATHER_DELAY'].sum()
         delay2 = df_routes['NAS_DELAY'].sum()
         delay3 = df_routes['SECURITY_DELAY'].sum()
         delay4 = df_routes['LATE_AIRCRAFT_DELAY'].sum()
+        #Sum total delay by carrier
         total_delay = delay0 + delay1 + delay2 + delay3 + delay4
+        #Append each cause to list as tuple along with number of minutes of delay, and percentage of total delay
         delays.append((delay0, delay0 / total_delay * 100, 'Carrier'))
         delays.append((delay1, delay1 / total_delay * 100, 'Weather'))
         delays.append((delay2, delay2 / total_delay * 100, 'National Air System'))
         delays.append((delay3, delay3 / total_delay * 100, 'Security'))
         delays.append((delay4, delay4 / total_delay * 100, 'Late Aircraft'))
+        #Sort list by number of minutes of delay, descending
         delays.sort(key=lambda x: x[0], reverse=True)
         
+        #TODO: Reformat text elements to be table-like with descriptive text as small header, numerical value as large table element for cleaner look
+        #Create output element, including text and plots. Use class names to fix element widths based on stylesheet
         output = html.Div([
             html.Div([
                 html.Div([
@@ -245,7 +316,6 @@ def update_airline(airline, date_range, tab_selected, selected_type):
                     html.H5(f'Average Departure Delay: {dep_delay:.0f} min'),
                     html.H5(f'Percentage of Flights Arriving Early/On-Time: {ontime_arr:.2f}%'),
                     html.H5(f'Percentage of Flights Departing Early/On-Time: {ontime_dep:.2f}%'),
-                    html.H5(f'Percentage of Flights Departing Late but Arriving Early/On-Time: {ontime_conv:.2f}% of delays'),
                 ],className='six columns'),
                 html.Div([
                     html.H5(f'Total Delay Caused by {delays[0][2]}: {delays[0][0]} min ({delays[0][1]:.2f}% of delays)'),
@@ -259,19 +329,13 @@ def update_airline(airline, date_range, tab_selected, selected_type):
                 html.Div([
                     dcc.Graph(figure=px.line(daily_avg_delay, x='FL_DATE', y=['ARR_DELAY', 'DEP_DELAY'], markers=True))
                 ]),
-            ]),
-            html.Div([
-                html.Div([
-                    dcc.Graph(figure=px.histogram(df_routes, x='FL_DATE'))
-                ], className='six columns'),
-                html.Div([
-                    dcc.Graph(figure=px.line(days_of_week, x='Day of Week', y='Number of Flights', hover_name='Day Name')),
-                ], className='six columns')
-            ], className='twelve columns'),
+            ], className='twelve columns')
         ])
-
+    #Return the output div
     return output
 
+#(Clientside) Callback 3 for changing the title of the webpage based on if a tab is selected.
+# If a tab is not selected, use a descriptive title. If a tab is selected, use the name of the tab as the title. Uses "Loading..." as title while processing
 app.clientside_callback(
     '''
     function(tab_value) {
@@ -287,6 +351,6 @@ app.clientside_callback(
     Input('main-tab-selector', 'value')
 )
 
-#Run with debug mode active on port 8080
+#Run on port 8080
 if __name__ == '__main__':
     application.run_server(debug=False, port=8080)
